e40e583609b59b1fe808aa78c2c893ce826fb7b4
Mohammad Ali Yazdani <mohammadaliyazdani21@gmail.com>
2025-02-20 10:00:00 +0300

    feat: add main.js script

---
+++ src/main.js
@@ -0,0 +1,45 @@
+import * as THREE from "three";
+
+// Scene and Camara
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+camera.position.z = 5;
+
+// Renderer
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+// Light
+const light = new THREE.AmbientLight(0xffffff, 1);
+scene.add(light);
+
+// Texture Loader
+const textureLoader = new THREE.TextureLoader();
+const earthTexture = textureLoader.load("public/textures/earth/earth.jpg");
+
+// Create Sphere
+const geometry = new THREE.SphereGeometry(1, 64, 64);
+const material = new THREE.MeshStandardMaterial({ map: earthTexture });
+const earth = new THREE.Mesh(geometry, material);
+scene.add(earth);
+
+// Rotate Animation
+function animate() {
+  requestAnimationFrame(animate);
+  earth.rotation.y += 0.005;
+  renderer.render(scene, camera);
+}
+animate();
+
+// Resize Handler
+window.addEventListener("resize", () => {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+});
